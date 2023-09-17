d7c467eac92d5934fe25ea116fe63b3d72c690b7
Muhammad Bilal <muhammadbilalll2468@gmail.com>
2023-09-17 17:29:45 +0800

    update

--- src/layouts/Form.jsx
+++ src/layouts/Form.jsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router";
 
 const Form = () => {
   return (
-    <div className="flex justify-center min-h-screen w-full py-5 px-5 bg-slate-200">
-      <div className="w-3/6 border-2 border-solid border-yellow-300 border-t-8 rounded-xl shadow-xl bg-white p-7">
+    <div className="flex justify-center min-h-screen w-full xl:py-5 xl:px-5 sm:py-2 sm:px-2 bg-slate-200">
+      <div className="xl:w-3/6 sm:w-6/6 border-2 border-solid border-yellow-300 border-t-8 rounded-xl shadow-xl bg-white p-7">
         <h3 className="text-3xl font-semibold mb-10">
           HAZARD INFORMATION AND SUGGESTION (HIAS) (Saran dan Informasi Bahaya)
         </h3>
--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -6,13 +6,13 @@ import "../index.css";
 const LandingPage = () => {
   return (
     <>
-      <div className="grid grid-rows-6 px-32 py-10 h-screen">
+      <div className="grid grid-rows-6 xl:px-32 py-10 h-screen sm:px-7">
         <div className="row-span-1 flex justify-between ">
           <img src={logoImg} alt="" className="w-32 h-10" />
           <p></p>
         </div>
         <div className="row-span-5 flex gap-11">
-          <div className=" flex flex-col gap-6  justify-center ">
+          <div className=" flex flex-col gap-6  justify-center md:items-start sm:items-center ">
             <h3 className="text-3xl font-bold">
               HAZARD INFORMATION AND SUGGESTION{" "}
               <span className="text-yellow-300">
@@ -30,7 +30,7 @@ const LandingPage = () => {
               </button>
             </Link>
           </div>
-          <img src={bgImg} alt="" className="" />
+          <img src={bgImg} alt="" className="md:block sm:hidden" />
         </div>
       </div>
     </>
